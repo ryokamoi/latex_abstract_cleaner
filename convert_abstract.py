8d0bc6bbf3a84c576c8f4712a29d1f98682a3150
ryokamoi <ryokamoi.jp@gmail.com>
2024-11-15 01:48:17 -0500

    updated

--- convert_abstract.py
+++ convert_abstract.py
@@ -3,14 +3,17 @@ import re
 if __name__ == "__main__":
     with open("original.txt", "r") as f:
         abstract = f.read()
+
+    # replace
+    replace_list = [["\\visonlyqa", "VisOnlyQA"]]
+    for replace in replace_list:
+        abstract = abstract.replace(replace[0], replace[1])
     
     # remove comments (row after %)
     abstract = re.sub(r"%.*", "", abstract)
     
-    # remove texts in \rui{} and \ryo{}
-    # e.t., "\ryo{this is a comment}" -> ""
-    for func in ["rui", "ryo"]:
-        abstract = re.sub(r"\\{}{{.*?}}".format(func), "", abstract)
+    # remove texts like \rui{} and \ryo{}
+    abstract = re.sub(r"\\[a-z]+{.*?}", "", abstract)
     
     # replace to spaces
     replace_to_space = ["~"]
@@ -19,11 +22,6 @@ if __name__ == "__main__":
     # remove
     remove_list = ["{}"]
     abstract = re.sub("|".join(remove_list), "", abstract)
-    
-    # replace
-    replace_list = [["\\visonlyqa", "VisOnlyQA"]]
-    for replace in replace_list:
-        abstract = abstract.replace(replace[0], replace[1])
     
     # remove line breaks
     abstract = abstract.replace("\n", " ")
