912677591db6a36617da7ab46542a1c81b0b2912
ryokamoi <ryokamoi.jp@gmail.com>
2024-12-01 14:54:08 -0500

    fixed to keep url

--- convert_abstract.py
+++ convert_abstract.py
@@ -1,11 +1,22 @@
 import re
+import csv
 
 if __name__ == "__main__":
     with open("original.txt", "r") as f:
         abstract = f.read()
 
+    # read replace list from CSV file
+    replace_list = []
+    with open("replace_list.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row) == 2:
+                replace_list.append(row)
+    
+    # add hardcoded replacements
+    replace_list.append(["---", "&mdash;"])
+    
     # replace
-    replace_list = [["\\visonlyqa", "VisOnlyQA"]]
     for replace in replace_list:
         abstract = abstract.replace(replace[0], replace[1])
     
@@ -13,7 +24,11 @@ if __name__ == "__main__":
     abstract = re.sub(r"%.*", "", abstract)
     
     # remove texts like \rui{} and \ryo{}
-    abstract = re.sub(r"\\[a-z]+{.*?}", "", abstract)
+    # but do not remove \url{}
+    abstract = re.sub(r'\\(?!url\b)\w+\{.*?\}', "", abstract)
+    
+    # \url{text} -> text
+    abstract = re.sub(r'\\url\{(.+?)\}', r'\1', abstract)
     
     # replace to spaces
     replace_to_space = ["~"]
